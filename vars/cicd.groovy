0a3902bf4cffd5e260453d2ae0fe13c3fe5c4dfe
lnarayanareddy <113675100+lnarayanareddy@users.noreply.github.com>
2023-01-05 16:59:58 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,13 +2,3 @@ def newGit(repo)
 {
    git "https://github.com/lnarayanareddy/${repo}.git"
 }
-
-def newMaven()
-{
-   sh 'mvn package'
-}
-
-def luckyDeploy(jobname,ipadress,context)
-{
-   sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipadress}:/var/lib/tomcat9/webapps/${context}.war"
-}
