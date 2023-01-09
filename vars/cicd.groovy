7966dee98d3cb58b091bb15ef8ac73c78d5a7bd0
lnarayanareddy <113675100+lnarayanareddy@users.noreply.github.com>
2023-01-09 17:08:14 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,8 +7,3 @@ def newMaven()
 {
     sh 'mvn package'
 }
-
-def newDeploy(jobname,ipaddress,context)
-{
-  sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
-}
