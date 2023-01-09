5c3778dc1dad5e3a4be92ccaec103b08ea5542e7
lnarayanareddy <113675100+lnarayanareddy@users.noreply.github.com>
2023-01-09 16:33:29 +0530

    Update jenkins.groovy

--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -7,3 +7,7 @@ def newMaven()
 {
     sh 'mvn package'
 }
+def newDeploy(jobname,ipaddress)
+{
+  sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/rangoli.war"
+}
